ff4972ecd3bb7b7d7b0b1140823d7b6815721e37
Richard Howlett <richardhowlett14@gmail.com>
2020-02-23 23:37:49 -0500

    File List Complete

--- app/views.py
+++ app/views.py
@@ -50,12 +50,22 @@ def upload():
 
     return redirect(url_for('home'))
 
-"""
-@app.route('/files/')
+def get_uploaded_images():
+    rootdir = os.getcwd()
+    print(rootdir)
+    lst = []
+    for subdir, dirs, files in os.walk(rootdir + '/app/static/uploads'):
+        for file in files:
+            lst.append(file)
+        """print os.path.join(subdir, file)"""
+    return lst 
+
+
+@app.route('/files')
 def files():
     if not session.get('logged_in'):
          abort(401)
-    return render_template('files.html', file = get_uploaded_images())"""
+    return render_template('files.html', file = get_uploaded_images())
 
 
 @app.route('/login', methods=['POST', 'GET'])
